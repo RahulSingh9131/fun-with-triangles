5b80b47047e2b56b9e1fdf38ca95e24d0b2c8359
RAHUL SINGH <singh.rahul3111998vipin@gmail.com>
2021-08-23 12:22:59 +0530

    initial commit

---
+++ isTriangle.js
@@ -0,0 +1,20 @@
+const angleInput=document.querySelectorAll(".angle-input");
+const button=document.querySelector("#btn");
+const message=document.querySelector(".message");
+
+function checkSumOfAngles(angle1,angle2,angle3){
+    const angleSum=angle1+angle2+angle3;
+    return angleSum;
+}
+
+function istriangle(){
+        const angleSum=checkSumOfAngles(Number(angleInput[0].value),Number(angleInput[1].value),Number(angleInput[2].value));
+        if(angleSum===180){
+            message.innerText="yes the entered angles form a triangle.";
+        }else{
+            message.innerText="no the entered angles don't form a triangle.";
+        }
+ 
+}
+
+button.addEventListener("click",istriangle);
