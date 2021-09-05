a48bac00121ad9fb50b7f1c903a7671ee7416ba9
RAHUL SINGH <singh.rahul3111998vipin@gmail.com>
2021-09-05 13:16:15 +0530

    docs updated

--- isTriangle.js
+++ isTriangle.js
@@ -9,11 +9,16 @@ function checkSumOfAngles(angle1,angle2,angle3){
 
 function istriangle(){
         const angleSum=checkSumOfAngles(Number(angleInput[0].value),Number(angleInput[1].value),Number(angleInput[2].value));
-        if(angleSum===180){
-            message.innerText="yes the entered angles form a triangle.";
+        if(angleInput[0].value&&angleInput[1].value&&angleInput[2].value){
+            if(angleSum===180){
+                message.innerText="yes the entered angles form a triangle.";
+            }else{
+                message.innerText="no the entered angles don't form a triangle.";
+            }
         }else{
-            message.innerText="no the entered angles don't form a triangle.";
+            message.innerText="Please enter all the fields.."
         }
+     
  
 }
 
